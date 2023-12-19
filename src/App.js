2a70bd2e3ecf5697972ab2b8af17165afa1a896f
MD FARHAN <50393822+mdfarhan0203@users.noreply.github.com>
2023-12-19 15:37:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const apiKey = "324be0d391437b1f0493ea8d8fa50b58";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+export default function App() {
+  const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
+  const [error, setError] = useState(null);
+
+  async function getWeatherInfo(city) {
+    try {
+      const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+      const result = await response.json();
+      if (result.cod == 200) {
+        setWeatherData(result);
+        setError(null);
+      } else {
+        setError(result.message + " Try Again");
+        setWeatherData(null);
+      }
+    } catch (error) {
+      setError(error.message);
+      setWeatherData(null);
+    }
+  }
+
+  const searchHandler = async () => {
+    await getWeatherInfo(city);
+    setCity("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card">
+        <div className="inbut-box">
+          <input
+            className="input-field"
+            type="text"
+            placeholder="Enter Place Name"
+            name="city"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+          />
+          <button className="search-btn" onClick={searchHandler}>
+            <img src="./images/search.png" alt="search icon" />
+          </button>
+        </div>
+
+        {/* if city Name is wrong  */}
+        {error && (
+          <div className="weather">
+            <p className="error">{error}</p>
+          </div>
+        )}
+
+        {/* If Weather  Data   if there */}
+        {weatherData && (
+          <div className="weather">
+            <img
+              src={`./images/${weatherData.weather[0].main}.png`}
+              alt="weather icon"
+              className="weather-icon"
+            />
+            <h1 className="temp">{Math.round(weatherData.main.temp)} °C</h1>
+            <h2 className="city-name">{weatherData.name}</h2>
+
+            <div className="details">
+              <div className="col">
+                <img src="./images/humidity.png" />
+                <div>
+                  <p className="humidity">{weatherData.main.humidity}</p>
+                  <p>Humidity</p>
+                </div>
+              </div>
+              <div className="col">
+                <img src="./images/wind.png" />
+                <div>
+                  <p className="wind">{weatherData.wind.speed} KM/h</p>
+                  <p>Wind Speed</p>
+                </div>
+              </div>
+            </div>
+            <div>
+              <h2 className="desc">{weatherData.weather[0].description}</h2>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
-
-export default App;
